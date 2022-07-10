3c04b1610d0f34c1cbb91ebb71256cddc6877f3e
tamadam <60942087+tamadam@users.noreply.github.com>
2022-07-10 17:18:43 +0200

    Base

---
+++ src/Szakdolgozat/routing.py
@@ -0,0 +1,34 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+from public_chat.consumers import PublicChatRoomConsumer
+
+"""
+###   Django channels kapcsolódó dokumentáció: https://channels.readthedocs.io/en/latest/topics/routing.html#   ###
+
+1. ProtocolTypeRouter : ennek segítségével definiálhatjuk milyen típusú ASGI appot készítünk [websocket típus]
+
+2. AllowedHostsOriginValidator : domainek halmaza, amelyek használhatják ezt a websocketet [ALLOWED_HOSTS]
+
+3. AuthMiddlewareStack :
+		request.user-el elérjük az adott usert a viewsban --> ugyanezt meg tudjuk csinálni a websocketes kódban is
+		--> self.scope['user'] : hozzáférést biztosít a user objecthez a sessionben a channel/websocket appon belül
+
+4. URLRouter : 	a url-eket itt arra használjuk, hogy megmondjuk a django websocket toolnak,
+				melyek azok a url-ek, amelyekhez websocketeket tudok csatlakoztatni
+				(azoknak a url-eknek a listája, ahova a websocketet csatlakoztatjuk, amíg nincs consumer addig üres)
+
+"""
+
+
+
+application = ProtocolTypeRouter({
+	'websocket': AllowedHostsOriginValidator(
+		AuthMiddlewareStack( 
+			URLRouter([
+				path('public_chat/<room_id>/', PublicChatRoomConsumer.as_asgi()),
+				]) 
+			)
+		),
+	})
---
+++ src/Szakdolgozat/urls.py
@@ -0,0 +1,58 @@
+"""Szakdolgozat URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from core.views import (
+    home_page_view,
+    users_search_view,
+    public_chat_page_view,
+    )
+
+from account.views import (
+    register_page_view,
+    login_page_view,
+    logout_view,
+    profile_view,
+    )
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # core app urls
+    path('', home_page_view, name='home_page'),
+    path('chat/', public_chat_page_view, name='public_chat'),
+
+
+    # account app urls
+    path('register/', register_page_view, name='register'),
+    path('login/', login_page_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('search/', users_search_view, name='search'),
+    path('profile/<user_id>/', profile_view, name='profile'),
+
+
+    # other urls inside account app
+    #path('account/', include('account.urls'))
+]
+
+# tell django where these staticfiles exists; urls host those resources
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
