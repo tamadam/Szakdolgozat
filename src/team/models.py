9f7b1df908f35b1dd76bd96efb14411f4932e6ec
tamadam <60942087+tamadam@users.noreply.github.com>
2022-07-18 21:02:22 +0200

    Database reset

--- src/team/models.py
+++ src/team/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Team(models.Model):
+	name 			= models.CharField(max_length=60, unique=True, blank=False)
+	description		= models.TextField(max_length=200, unique=False, blank=True, default="Leírás")
+	users 			= models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+
+	def __str__(self):
+		return self.name
+
+"""
+class Membership(models.Model):
+	users = 
+"""
