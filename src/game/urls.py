07a47cec2de277abb041f9ff9b9356e48668802e
tamadam <60942087+tamadam@users.noreply.github.com>
2022-09-12 15:45:08 +0200

    Full program 1.0
    Game choice page created 100%
    All main functions is working and all pages created with design

--- src/game/urls.py
+++ src/game/urls.py
@@ -14,9 +14,9 @@ app_name = 'game'
 
 urlpatterns = [
 	path('', game_choice_view, name='game_choice'),
-	path('konnyu/', easy_game_view, name='easy_game'),
-	path('kozepes/', medium_game_view, name='medium_game'),
-	path('nehez/', hard_game_view, name='hard_game'),
+	path('memoria/', easy_game_view, name='easy_game'),
+	path('puzzle/', medium_game_view, name='medium_game'),
+	path('aknakereso/', hard_game_view, name='hard_game'),
 	path('finished_game',finished_game, name='finished_game'),
 	path('arena/', arena_view, name='arena'),
 	path('arena_fight', arena_fight, name='arena_fight'),
